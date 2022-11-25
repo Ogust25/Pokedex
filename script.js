9a8c5446b141e0b9b4d3f6ec0abd2ff2579d4143
Ogu <august.gros@gmail.com>
2022-11-25 18:24:45 +0100

    add some style

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const pokeImg = document.querySelector('#pokeImg');
 const pokeName = document.querySelector('#pokeName');
 const pokeType1 = document.querySelector('#pokeType1');
 const pokeType2 = document.querySelector('#pokeType2');
+const pokeType1Color = document.querySelector('#pokeType1Color');
+const pokeType2Color = document.querySelector('#pokeType2Color');
 const pokeHeight = document.querySelector('#pokeHeight');
 const pokeWeight = document.querySelector('#pokeWeight');
 const pokeAbilitiesContainer = document.querySelector('#pokeAbilitiesContainer');
@@ -35,12 +37,15 @@ function apiCall(search) {
         pokeImg.src = data.sprites.front_default;
 
         pokeName.firstChild.nodeValue = upperFirstChar(data.name);
-        pokeType1.firstChild.nodeValue = upperFirstChar(data.types[0].type.name);
 
+        pokeType1.firstChild.nodeValue = upperFirstChar(data.types[0].type.name);
+        colorType(data.types[0].type.name, pokeType1Color);
         if (data.types[1] === undefined) {
             pokeType2.firstChild.nodeValue = "---";
+            pokeType2Color.style.backgroundColor = 'transparent';
         } else {
             pokeType2.firstChild.nodeValue = upperFirstChar(data.types[1].type.name);
+            colorType(data.types[1].type.name, pokeType2Color);
         }
 
         pokeHeight.firstChild.nodeValue = "Taille : " + data.height/10 + " m";
@@ -80,5 +85,66 @@ function newElem(el, attribut, content = "") {
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
+    }
+}
+
+function colorType(type, elem) {
+    switch (type) {
+        case "normal":
+            elem.style.backgroundColor = "#A8A77A";
+            break;
+        case "fire":
+            elem.style.backgroundColor = '#EE8130';
+            break;
+        case "water":
+            elem.style.backgroundColor = '#6390F0';
+            break;
+        case "electric":
+            elem.style.backgroundColor = '#F7D02C';
+            break;
+        case "grass":
+            elem.style.backgroundColor = '#7AC74C';
+            break;
+        case "ice":
+            elem.style.backgroundColor = '#96D9D6';
+            break;
+        case "fighting":
+            elem.style.backgroundColor = '#C22E28';
+            break;
+        case "poison":
+            elem.style.backgroundColor = '#A33EA1';
+            break;
+        case "ground":
+            elem.style.backgroundColor = '#E2BF65';
+            break;
+        case "flying":
+            elem.style.backgroundColor = '#A98FF3';
+            break;
+        case "psychic":
+            elem.style.backgroundColor = '#F95587';
+            break;
+        case "bug":
+            elem.style.backgroundColor = '#A6B91A';
+            break;
+        case "rock":
+            elem.style.backgroundColor = '#B6A136';
+            break;
+        case "ghost":
+            elem.style.backgroundColor = '#735797';
+            break;
+        case "dragon":
+            elem.style.backgroundColor = '#6F35FC';
+            break;
+        case "dark":
+            elem.style.backgroundColor = '#705746';
+            break;
+        case "steel":
+            elem.style.backgroundColor = '#B7B7CE';
+            break;
+        case "fairy":
+            elem.style.backgroundColor = '#D685AD';
+            break;
+        default:
+            break;
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","script.js"],
+  content: ["./index.html", "./script.js"],
   theme: {
     extend: {
       colors: {
         pokeRed: '#D21029',
+        pokeBlue: '#00A8C2',
+        normal: '#A8A77A',
+        fire: '#EE8130',
+        water: '#6390F0',
+        electric: '#F7D02C',
+        grass: '#7AC74C',
+        ice: '#96D9D6',
+        fighting: '#C22E28',
+        poison: '#A33EA1',
+        ground: '#E2BF65',
+        flying: '#A98FF3',
+        psychic: '#F95587',
+        bug: '#A6B91A',
+        rock: '#B6A136',
+        ghost: '#735797',
+        dragon: '#6F35FC',
+        dark: '#705746',
+        steel: '#B7B7CE',
+        fairy: '#D685AD',
       },
+      boxShadow: {
+        'pokeInner': 'inset 0px 0px 4px 8px rgba(0, 0, 0, 0.13)',
+      }
+    },
+    fontFamily: {
+      'pokeFont': "PKMN RBYGSC",
     },
   plugins: [],
 }}
