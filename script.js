9e9c537ed32ebe39e9affe5c0d3c6a9bd81bf810
Ogu <august.gros@gmail.com>
2022-11-25 16:45:01 +0100

    fetch all we need

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const pokeType1 = document.querySelector('#pokeType1');
 const pokeType2 = document.querySelector('#pokeType2');
 const pokeHeight = document.querySelector('#pokeHeight');
 const pokeWeight = document.querySelector('#pokeWeight');
+const pokeAbilitiesContainer = document.querySelector('#pokeAbilitiesContainer');
+const pokeMovesContainer = document.querySelector('#pokeMovesContainer');
 
 // Event click
 btnSearch.addEventListener('click', () => {
@@ -43,10 +45,40 @@ function apiCall(search) {
 
         pokeHeight.firstChild.nodeValue = "Taille : " + data.height/10 + " m";
         pokeWeight.firstChild.nodeValue = "Poids : " + data.weight/10 + " kg";
+
+        removeAllChildNodes(pokeAbilitiesContainer);
+        for (let i = 0; i < data.abilities.length; i++) {
+            let ability = newElem("p", {class: ''}, upperFirstChar(data.abilities[i].ability.name));
+            pokeAbilitiesContainer.insertBefore(ability, null);
+        }
+
+        removeAllChildNodes(pokeMovesContainer);
+        for (let i = 0; i < data.moves.length; i++) {
+            let ability = newElem("p", {class: ''}, upperFirstChar(data.moves[i].move.name));
+            pokeMovesContainer.insertBefore(ability, null);
+        }
     
     })
 )}
 
+/* Fonction pour ajouter la 1ere lettre en Majuscule */
 function upperFirstChar(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* Fonction pour créer des elements */
+function newElem(el, attribut, content = "") {
+    const newElem = document.createElement(el);
+    const newContent = document.createTextNode(content);
+    newElem.appendChild(newContent);
+    for (let key in attribut) {
+      newElem.setAttribute(key, attribut[key]);
+    }
+    return newElem;
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
